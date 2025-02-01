466cbbed62c26a6e6f2d8e4f39868d738556b24d
SBez <sergei.bezymyanniy@gmail.com>
2025-02-01 13:31:03 +0300

    Update func.py

--- func.py
+++ func.py
@@ -1,43 +1,53 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext, Updater, CommandHandler, CallbackQueryHandler
 
+#Функция, очищающая сообщения бота от лишних символов
 def clean_result (result):
     result = result.replace('{"result":{"alternatives":[{"message":{"role":"assistant","text":"',' ')
-    result = result.split('"status"')[0].strip()
+    result = result.split('"status"')[0].strip().replace('}','')
     result = result.replace('$','').replace('\\Delta','Δ').replace('frac', '')
-    result = result.replace('\\n','').replace('\\','').replace('}{','/').replace('}','')
+    result = result.replace('\\n','').replace('\\','').replace('}{','/')
     result = result.replace('{','').replace('int', '∫') [:-2]
+    return result
 
+#Команда /codif
 def codif(update: Update, context: CallbackContext):
     keyboard = [
-        [InlineKeyboardButton("ОГЭ", callback_data="OGE"), InlineKeyboardButton("ЕГЭ", callback_data="EGE")]
+        [InlineKeyboardButton("ОГЭ",
+                              callback_data="OGE"),
+         InlineKeyboardButton("ЕГЭ", callback_data="EGE")
+         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     
-    update.message.reply_text("Для какого экзамена вам нужен кодификатор?", reply_markup=reply_markup)
-
+    update.message.reply_text("Для какого экзамена вам нужен кодификатор?",
+                              reply_markup=reply_markup)
 def send_codif(update: Update, context: CallbackContext):
     query = update.callback_query
-    exam_type = query.data  # Получаем, какую кнопку нажал пользователь
+    exam_type = query.data
     chat_id = query.message.chat_id
 
     if exam_type == "OGE":
-        file_path = "fi-9_oge_2025_kodif.pdf"  # Путь к кодификатору ОГЭ
+        codif_file = "fi-9_oge_2025_kodif.pdf"
         message = "Отправляю кодификатор для ОГЭ."
     elif exam_type == "EGE":
-        file_path = "code-2024.pdf"  # Путь к кодификатору ЕГЭ
+        codif_file = "code-2024.pdf"
         message = "Отправляю кодификатор для ЕГЭ."
 
-    context.bot.send_document(chat_id=chat_id, document=open(file_path, 'rb'))
+    context.bot.send_document(chat_id=chat_id, document=open(codif_file, 'rb'))
     query.edit_message_text(message)
-    
+
+#Команда /start
 def start(update: Update, context):
     update.message.reply_text("Привет, я помогу тебе с изучением физики!")
 
+#Команда /help
 def help(update: Update, context):
     update.message.reply_text("1. /codif - отправляет кодификатор для ЕГЭ/ОГЭ по физике;"
-                              "\n2. /recommend - отправляет список рекомендованных материалов для изучения физики")
+                              "\n2. /recommend - отправляет список рекомендованных материалов для изучения физики"
+                              "\n3. /ExamDemo - отправляет демоверсию ЕГЭ/ОГЭ по физике")
 
+#Команда /recommend
 def recommend(update: Update, context):
     update.message.reply_text("1. Г.С.Ландсберг - элементарный учебник физики в трёх томах;"
                               "\n2. Задачник А.П Рымкевича 10-11 кл;"
@@ -46,3 +56,30 @@ def recommend(update: Update, context):
                               "https://3.shkolkovo.online/catalog);"
                               "\n5. Купер Л. Физика для всех. Классическая физика;"
                               "\n6. Чешев Ю.В ., Можаев В.В и др. Методическое пособие для поступающих в ВУЗ.")
+
+#Команда /ExamDemo
+def exam_demo(update: Update, context: CallbackContext):
+    keyboard = [
+        [InlineKeyboardButton("ОГЭ",
+                              callback_data="OGE"),
+         InlineKeyboardButton("ЕГЭ", callback_data="EGE")
+         ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    update.message.reply_text("Для какого экзамена вам нужна демоверсия?",
+                              reply_markup=reply_markup)
+def send_exam_demo(update: Update, context: CallbackContext):
+    query = update.callback_query
+    exam_type = query.data
+    chat_id = query.message.chat_id
+
+    if exam_type == "OGE":
+        codif_file = "физика-демо.pdf"
+        message = "Отправляю кодификатор для ОГЭ."
+    elif exam_type == "EGE":
+        codif_file = "демо-фи.pdf"
+        message = "Отправляю демо для ЕГЭ."
+
+    context.bot.send_document(chat_id=chat_id, document=open(codif_file, 'rb'))
+    query.edit_message_text(message)
