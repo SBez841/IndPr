b7ceb7366cb9649fcaa3eb85824eace0d0a59ddf
SBez <sergei.bezymyanniy@gmail.com>
2025-02-06 19:49:21 +0300

    Update YAGPT.py

--- YAGPT.py
+++ YAGPT.py
@@ -1,12 +1,8 @@
 import requests
-from consts import API_Key, Folder_ID, PROMPT #Импорт ключей и промпта для нейросети из файла
-from func import clean_result
+from consts import API_Key, Folder_ID, PROMPT #Импорт ключей и промпта для бота
+from func import clean_result #Импорт функции для очистки сообщения бота
 
-def load_tasks(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read()
-
-#Промпты нейросети
+#Функция обращения к YandexGPT
 def  get_response(history):
     prompt = {
         'modelUri':'gpt://'+Folder_ID+'/yandexgpt-lite/latest',
@@ -15,21 +11,21 @@ def  get_response(history):
             'temperature': 0.6,
             'maxTokens':3000
             },
-
         'messages':[
             {'role' : 'system',
             'text' : PROMPT},
         ]
     }
+    
     prompt['messages'].extend(history)
     url='https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
     headers = {
         'Content-Type' : 'application/json',
         'Authorization': 'Api-Key '+API_Key
     }
-
+    
     response = requests.post(url, headers=headers, json=prompt)
     result=response.text
-    result=clean_result(result) #Использование функции clean_result для очистки текста нейросети от лишних символов
-     
+    result=clean_result(result)
+    
     return result
